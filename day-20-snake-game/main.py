2b328bdbd76b0ac21dceb2271ef0e769fee17627
Ngoc <dungtuyetngoc@gmail.com>
2022-10-11 22:01:52 +0700

    added move in 4 directions for snake

--- day-20-snake-game/main.py
+++ day-20-snake-game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake #import snake class
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,35 +8,22 @@ screen.bgcolor("black")
 screen.title("My snake game")
 screen.tracer(0)
 
-starting_pos = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
 
+snake = Snake()
 
-def init_turtle(pos):
-    t = Turtle()
-    t.shape("square")
-    t.color("white")
-    t.penup()
-    t.goto(pos)
-    return t
+screen.listen()
+screen.onkey(fun=snake.up,    key="Up")
+screen.onkey(fun=snake.down,  key="Down")
+screen.onkey(fun=snake.left,  key="Left")
+screen.onkey(fun=snake.right, key="Right")
 
-
-for pos in starting_pos:
-    segments.append(init_turtle(pos))
-screen.update()
 
 game_is_on = True
 
 while game_is_on:
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
-
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
 
 screen.exitonclick()
