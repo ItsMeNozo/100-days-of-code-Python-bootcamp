d3066c1a78cf939e792195d4e29ec19610709880
Ngoc <dungtuyetngoc@gmail.com>
2022-11-26 20:48:44 +0700

    finished day 25

--- day-25-csv-data/main.py
+++ day-25-csv-data/main.py
@@ -16,24 +16,54 @@
 #             temperature.append(int(row[1]))
 #     print(temperature)
 
-import pandas
+# import pandas
 
-data = pandas.read_csv("weather_data.csv") # pandas read file for you, no need to open file
-temp_series = data["temp"]
+# data = pandas.read_csv("weather_data.csv") # pandas read file for you, no need to open file
+# temp_series = data["temp"]
 # temp_list = data["temp"].to_list()
 # avg_temp = sum(temp_list)/len(temp_list)
 # print(avg_temp)
 # print(data[data.temp == data.temp.max()]) # print row with max temp
 # print(data[data.day == "Monday"].condition)
-monday_temp = int(data[data.day == "Monday"].temp)
-monday_temp_f = monday_temp * 1.8 + 32
+# monday_temp = int(data[data.day == "Monday"].temp)
+# monday_temp_f = monday_temp * 1.8 + 32
 # print(monday_temp_f)
 
 # Create DataFrame from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-data = pandas.DataFrame(data_dict)
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
 # export csv file as "new_data.csv"
-data.to_csv("new_data.csv")
+# data.to_csv("new_data.csv")
+import pandas
+
+# Task: extract a cvs file of fur color count in Primary fur color column
+# data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# fur_color_list = data[data["Primary Fur Color"].isnull() == False]["Primary Fur Color"].to_list()
+# fur_count_dict = {}
+#
+# for color in fur_color_list:
+#     # if color.isnull():
+#     #     continue
+#     fur_count_dict[color] = fur_color_list.count(color)
+# unique_fur_color_list = []
+# count_list = []
+# for color in fur_count_dict:
+#     unique_fur_color_list.append(color)
+#     count_list.append(fur_count_dict[color])
+# csv_dict = {
+#     "Fur Color": unique_fur_color_list,
+#     "Count": count_list
+# }
+#
+# fur_count_data = pandas.DataFrame(csv_dict)
+# fur_count_data.to_csv("squirrel_count.csv")
+
+# shorter code
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+color_count = data["Primary Fur Color"].value_counts()
+color_count.to_csv("squirrel_count_fast.csv")
---
+++ day-25-csv-data/us-states-game-start/us-states-game-start/main.py
@@ -0,0 +1,37 @@
+import turtle
+import pandas
+
+# ref: https://stackoverflow.com/questions/42878641/get-mouse-click-coordinates-in-python-turtle
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+guessed_states = [] # to print all missing states later
+data = pandas.read_csv("50_states.csv")
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(f"{len(guessed_states)}/50 U.S States Correct", "What's another state name?")
+    state_line = data[data.state.str.lower() == answer_state.lower()]
+
+    if answer_state.lower() == "exit":
+        states_not_guessed = []
+        for state in data.state:
+            if state not in guessed_states:
+                states_not_guessed.append(state)
+        not_guessed_state_dataFrame = pandas.DataFrame({"states": states_not_guessed})
+        not_guessed_state_dataFrame.to_csv("states_to_learn.csv")
+        break
+    if state_line.empty:
+        continue
+    state = state_line.state.item()  # Item return the first element
+    if state in guessed_states:
+        continue
+    guessed_states.append(state)
+    t = turtle.Turtle()
+    t.penup()
+    t.hideturtle()
+    t.goto(int(state_line.x), int(state_line.y))
+    t.write(state)
+
